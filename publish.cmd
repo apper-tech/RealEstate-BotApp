df87d8071800a8e48246f6f318eb0034de5a24ff
Mh Mazen <mcs3dp7@hotmail.com>
2018-10-23 14:02:57 +0300

    Delete

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild RealEstateBot.sln -p:DeployOnBuild=true -p:PublishProfile=realestate-bot-Web-Deploy.pubxml -p:Password=a3rfwQbQmTsiTGsRQCHmcZFH9E4kJxJZihkg5NTwwpsSsTY8XHem6ADDyjGi
-
