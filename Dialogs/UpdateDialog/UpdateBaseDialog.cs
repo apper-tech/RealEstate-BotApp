43702e5a86caead8d148423d17658c00fa86de6e
Mh Mazen <mcs3dp7@hotmail.com>
2018-10-28 18:35:04 +0200

    test

--- Dialogs/UpdateDialog/UpdateBaseDialog.cs
+++ Dialogs/UpdateDialog/UpdateBaseDialog.cs
@@ -20,8 +20,15 @@ namespace AkaratakBot.Dialogs.UpdateDialog
         public async Task ShowPropertyList(IDialogContext context)
         {
             var reply = context.MakeMessage();
-            reply.AttachmentLayout = AttachmentLayoutTypes.List;
-            reply.Attachments = Shared.Common.Update.GetPropertyList(Shared.API.IOCommon.UserManager.GetUserID(_userProfile,true));
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            //reply.Attachments = Shared.Common.Update.GetPropertyList(Shared.API.IOCommon.UserManager.GetUserID(_userProfile,true));
+            reply.Attachments.Add(Shared.Cards.GetHeroCard(
+                                 "test",
+                                "test",
+                                 "test",
+                                 new CardImage(url: ("https://www.google.com/favicon.ico")),
+                                 new CardAction(ActionTypes.ImBack,"tet")
+                                 ));
             if (reply.Attachments.Count > 0)
                 await context.PostAsync(reply);
             else
