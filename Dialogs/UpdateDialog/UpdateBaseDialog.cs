b67a2f1aa814bb3e988ca0c45c45174c15f5534f
Mh Mazen <mcs3dp7@hotmail.com>
2018-10-29 15:06:24 +0200

    test List

--- Dialogs/UpdateDialog/UpdateBaseDialog.cs
+++ Dialogs/UpdateDialog/UpdateBaseDialog.cs
@@ -19,20 +19,10 @@ namespace AkaratakBot.Dialogs.UpdateDialog
         UserProfile _userProfile;
         public async Task ShowPropertyList(IDialogContext context)
         {
-            var reply = context.MakeMessage();
-            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
-            //reply.Attachments = Shared.Common.Update.GetPropertyList(Shared.API.IOCommon.UserManager.GetUserID(_userProfile,true));
-            reply.Attachments.Add(Shared.Cards.GetHeroCard(
-                                 "test",
-                                "test",
-                                 "test",
-                                 new CardImage(url: "tstsd"),
-                                 new CardAction(ActionTypes.OpenUrl, "tet")
-                                 ));
-            //if (reply.Attachments.Count > 0)
-                await context.PostAsync(reply);
-            //else
-            await context.PostAsync("Nothing to Update2");
+            var replyMessage = context.MakeMessage();
+            Attachment attachment = GetProfileHeroCard(); ;
+            replyMessage.Attachments = new List<Attachment> { attachment };
+            await context.PostAsync(replyMessage);
             context.Wait<Activity>(AfterPropertyList);
         }
         public async Task AfterPropertyList(IDialogContext context,IAwaitable<Activity> argument)
@@ -44,6 +34,25 @@ namespace AkaratakBot.Dialogs.UpdateDialog
             //save and ask 
 
         }
-      
+        private static Attachment GetProfileHeroCard()
+        {
+            var heroCard = new HeroCard
+            {
+                // title of the card  
+                Title = "Suthahar Jegatheesan",
+                //subtitle of the card  
+                Subtitle = "Microsoft certified solution developer",
+                // navigate to page , while tab on card  
+                Tap = new CardAction(ActionTypes.OpenUrl, "Learn More", value: "http://www.devenvexe.com"),
+                //Detail Text  
+                Text = "Suthahar J is a Technical Lead and C# Corner MVP. He has extensive 10+ years of experience working on different technologies, mostly in Microsoft space. His focus areas are  Xamarin Cross Mobile Development ,UWP, SharePoint, Azure,Windows Mobile , Web , AI and Architecture. He writes about technology at his popular blog http://devenvexe.com",
+                // list of  Large Image  
+                Images = new List<CardImage> { new CardImage("http://csharpcorner.mindcrackerinc.netdna-cdn.com/UploadFile/AuthorImage/jssuthahar20170821011237.jpg") },
+                // list of buttons   
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "http://www.devenvexe.com"), new CardAction(ActionTypes.OpenUrl, "C# Corner", value: "http://www.c-sharpcorner.com/members/suthahar-j"), new CardAction(ActionTypes.OpenUrl, "MSDN", value: "https://social.msdn.microsoft.com/profile/j%20suthahar/") }
+            };
+
+            return heroCard.ToAttachment();
+        }
     }
 }
